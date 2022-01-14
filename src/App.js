eeeab57b1f176b438747116792d4b2ddf0719abb
Fatih <fatihce57@gmail.com>
2022-01-14 02:05:47 +0100

    card created

--- src/App.js
+++ src/App.js
@@ -1,29 +1,32 @@
 import React, { useEffect, useState } from 'react';
-import './App.css';
+import './App.scss';
+import MovieList from './components/moviesPage/MovieList';
 
 function App() {
 
   const [movies, setMovies] = useState([])
 
   const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=fb610eb829d30acfac99f7dbaa1c937c&page=1'
+  const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=fb610eb829d30acfac99f7dbaa1c937c&query="'
 
   useEffect(() => {
     getAllMovies()
   }, []);
+
   const getAllMovies = async () => {
     const res = await fetch(`${API_URL}`);
     const data = await res.json();
     try {
       setMovies((prev) => [...prev, data.results])
+      console.log(data)
     } catch (err) {
       console.log(err)
     }
-    // console.log(getAllMovies())
   }
 
   return (
     <div className="App">
-      aaa
+      <MovieList movies={movies} />
     </div>
   );
 }
