cecca39e9a39c0c015af3e8f2e153755bdcf3312
Fatih <fatihce57@gmail.com>
2022-02-09 23:49:06 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './_App.scss';
 import { Routes, Route } from "react-router-dom";
 import MovieList from './components/movieList/MovieList';
@@ -11,23 +11,7 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [searchMovie, setSearchMovie] = useState('');
 
-  // const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=fb610eb829d30acfac99f7dbaa1c937c&page=1'
-  const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=fb610eb829d30acfac99f7dbaa1c937c&query="'
-
-  // useEffect(() => {
-  //   getAllMovies()
-  // }, []);
-
-  // const getAllMovies = async (e) => {
-  //   e.preventDefault();
-  //   const res = await fetch(`${API_URL}`);
-  //   const data = await res.json();
-  //   try {
-  //     setMovies((prev) => data.results)
-  //   } catch (err) {
-  //     console.log(err)
-  //   }
-  // }
+  const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=fb610eb829d30acfac99f7dbaa1c937c&query='
 
   const handleOnSubmit = async (e) => {
     e.preventDefault();
